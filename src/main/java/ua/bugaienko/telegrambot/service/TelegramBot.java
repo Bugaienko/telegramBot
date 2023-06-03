a01470ce251455e13c32980c45aa8fb71f9a5fed
Bugaienko <sergiibugaienko@gmail.com>
2023-06-03 21:20:26 +0200

    bots menu added

--- src/main/java/ua/bugaienko/telegrambot/service/TelegramBot.java
+++ src/main/java/ua/bugaienko/telegrambot/service/TelegramBot.java
@@ -4,10 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ua.bugaienko.telegrambot.config.BotConfig;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Sergii Bugaienko
@@ -19,9 +25,26 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     private final BotConfig config;
 
+    private static final String HELP_TEXT = "This is a bot-assistant to nutrition consultant Irina Bug.\n\n"
+            + "You can execute commands from the main menu on left or by typing a command:\n\n"
+            + "Type /start to see a welcome message\n"
+            + "Type /data  to see data stored about yourself\n"
+            + "Type /help to see this message again";
+
     @Autowired
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "Get a welcome message"));
+        listOfCommands.add(new BotCommand("/mydata", "get your data stored"));
+        listOfCommands.add(new BotCommand("/deletedata", "delete my data"));
+        listOfCommands.add(new BotCommand("/help", "info how to use this bot"));
+        listOfCommands.add(new BotCommand("/settings", "set your preferences"));
+        try {
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
+            log.error("Error setting bot's command list: " + e.getMessage());
+        }
     }
 
 
@@ -45,9 +68,13 @@ public class TelegramBot extends TelegramLongPollingBot {
 
             switch (messageText) {
                 case "/start":
-                startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                break;
-                default: sendMessage(chatId, "Sorry, command was not recognize :(");
+                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
+                    break;
+                case "/help":
+                    sendMessage(chatId, HELP_TEXT);
+                    break;
+                default:
+                    sendMessage(chatId, "Sorry, command was not recognize :(");
 
             }
 
@@ -71,7 +98,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         try {
             execute(message);
         } catch (TelegramApiException e) {
-           log.error("Error occurred: " + e.getMessage());
+            log.error("Error occurred: " + e.getMessage());
         }
     }
 
